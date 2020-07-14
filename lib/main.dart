053e312476def0bd9ce9a0407dc94dbc82f39618
Anant Kanchan <anant4265@gmail.com>
2020-07-14 13:08:07 +0530

    initial commit message

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
     home: Exp(),
   ));
 }
+
 class Exp extends StatefulWidget {
   @override
   _ExpState createState() => _ExpState();
@@ -15,24 +16,48 @@ class _ExpState extends State<Exp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Explore'),
-      ),
-      body: FlatButton(
-        color: Colors.blue,
-        onPressed: (){
-         FlutterToast(context).showToast(child: Text(
-           'You clicked on the button',
-         style: TextStyle(
-           color: Colors.red,
-           backgroundColor: Colors.blue
-         )),
-         toastDuration: Duration(seconds: 2),
-         gravity: ToastGravity.BOTTOM);
-        },
-        child: Text('Hello'),
-      )
+        appBar: AppBar(
+          title: Text('Explore'),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.white,
+                child: Padding(
+                    padding: const EdgeInsets.all(36.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(height: 45.0),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                labelText: 'Enter your username'),
+                          ),
+                          SizedBox(height: 25.0),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                labelText: 'Enter your password'),
+                          ),
+                          SizedBox(height: 45.0),
+                          FlatButton(
+                            color: Colors.blue,
+                            onPressed: () {
+                              FlutterToast(context).showToast(
+                                  child: Text('You clicked on the button',
+                                      style: TextStyle(
+                                          color: Colors.red,
+                                          backgroundColor: Colors.blue)),
+                                  toastDuration: Duration(seconds: 2),
+                                  gravity: ToastGravity.BOTTOM);
+                            },
+                            child: Text('Hello'),
+                          )
+
+                        ]
+                    )
+                )
+            )
+        )
     );
   }
 }
-
