b861e7f8972ae64cb7ef982ad0d4d8e7f90e3a4f
Anant Kanchan <anant4265@gmail.com>
2020-07-16 11:46:56 +0530

    initial commit message

---
+++ lib/login_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Exp extends StatefulWidget {
+  @override
+  _ExpState createState() => _ExpState();
+}
+
+class _ExpState extends State<Exp> {
+
+
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController=new TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Explore'),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.white,
+                child: Padding(
+                    padding: const EdgeInsets.all(36.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(height: 45.0),
+                          TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                                labelText: 'Enter your email'),
+
+                          ),
+                          SizedBox(height: 25.0),
+                          TextField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                                labelText: 'Enter your password'),
+
+                          ),
+                          SizedBox(height: 45.0),
+                          FlatButton(
+                            color: Colors.blue,
+                            onPressed: () {
+
+                              if(emailController.text=='anant'&& passwordController.text=='anant') {
+//                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => new_screen()));
+                                FlutterToast(context).showToast(
+                                    child: Text('Login successfull',
+                                        style: TextStyle(
+                                            color: Colors.red,
+                                            backgroundColor: Colors.grey,
+                                            letterSpacing: 1.0)
+                                    ),
+                                    toastDuration: Duration(seconds: 2),
+                                    gravity: ToastGravity.BOTTOM);
+//                              }
+                              }
+                              else{
+                                FlutterToast(context).showToast(
+                                    child: Text('Login unsuccessfull',
+                                        style: TextStyle(
+                                            color: Colors.red,
+                                            backgroundColor: Colors.grey,
+                                            letterSpacing: 1.0)
+                                    ),
+                                    toastDuration: Duration(seconds: 2),
+                                    gravity: ToastGravity.BOTTOM);
+                              }
+
+                            },
+                            child: Text('Next'),
+                          )
+                        ]
+                    )
+                )
+            )
+        )
+    );
+  }
+}
